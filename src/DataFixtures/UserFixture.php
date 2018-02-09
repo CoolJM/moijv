1163c8790b4cb4c83e049adcf51ab6f48fcc29c0
CoolJM <35835009+CoolJM@users.noreply.github.com>
2018-02-09 10:10:34 +0100

    Add product form and my products page

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -9,6 +9,15 @@ use Doctrine\Common\Persistence\ObjectManager;
 class UserFixture extends Fixture {
     
     public function load(ObjectManager $manager) {
+        // on créé un admin pour accéder à notre backoffice
+        $admin = new User();
+        $admin ->setRoles('ROLE_USER|ROLE_ADMIN');
+        $admin->setUsername('root');
+        $admin->setPassword(password_hash('root',PASSWORD_BCRYPT));
+        $admin->setEmail('admin@noreply.com'); 
+        $admin->setBirthdate(\DateTime::createFromformat('Y/m/d h:i:s', '2012/12/12 12:12:12'));
+        $manager->persist($admin);
+        
         // on créé une liste factice de 20 utilisateurs
         for($i = 1; $i <= 20; $i++){
             $user = new User();
@@ -16,6 +25,7 @@ class UserFixture extends Fixture {
             $user->setEmail('user'.$i.'@mail.com');
             $user->setFirstname('User'.$i);
             $user->setLastname('Fake'.$i);
+            $user->setRoles('ROLE_USER');
             $user->setPassword(password_hash('user'.$i,PASSWORD_BCRYPT));
             $user->setBirthdate(\DateTime::createFromformat('Y/m/d h:i:s', (2000 - $i).'/01/01 00:00:00'));
             
